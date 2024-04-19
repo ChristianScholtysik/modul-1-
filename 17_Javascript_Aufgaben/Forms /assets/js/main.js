d77838c12d619d965920373b11a85c61a3dbc624
Christian Scholtysik <christianscholtysik@gmail.com>
2024-04-19 14:10:47 +0200

    done

--- 17_Javascript_Aufgaben/Forms /assets/js/main.js
+++ 17_Javascript_Aufgaben/Forms /assets/js/main.js
@@ -16,3 +16,16 @@ function myAge() {
   outputAge.innerHTML = `Du  bist ${score} Jahre alt`;
   console.log(age);
 }
+
+//Forms-JS-Level-1_3
+function compareAge() {
+  const difference = document.querySelector("#difference");
+  const age1 = Number(document.querySelector("#age1").value);
+  const age2 = Number(document.querySelector("#age2").value);
+  //   console.log(age1);
+  //   console.log(age2);
+
+  const ergebnis = age1 - age2;
+  console.log(ergebnis);
+  difference.innerHTML = `Die Differenz beträgt: ${ergebnis}`;
+}
