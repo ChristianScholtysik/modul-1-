b04b730a7843b57765e89c328e54a7708a0345f3
Christian Scholtysik <christianscholtysik@gmail.com>
2024-04-19 13:35:48 +0200

    done

--- 17_Javascript/assets/js/main.js
+++ 17_Javascript/assets/js/main.js
@@ -40,13 +40,42 @@ function getInputValue() {
   let range = Number(document.querySelector(".range").value);
   let checkbox = document.querySelector(".checkbox").checked; //% checked fragt ob die Checkbox angewählt wurde. ich bekomme einen true oder false zurück
   let date = document.querySelector(".bday").value;
-  const output = document.querySelector("form-output");
+  const output = document.querySelector(".form-output");
 
   console.log({ name }, { age }, { range }, { checkbox }, { date });
-  console.log(name);
 
   output.innerHTML = ` ${name},${age}, ${range}, ${checkbox}, ${date}`;
 
   output.innerHTML =
     name + " " + age + " " + range + " " + checkbox + " " + date;
+}
+
+//? Conditional statements if/else
+
+//if else führt anweisungen nur aus wenn eine bedingung wahr ist, der optionale else zweig wird ausgeführt wenn die bedingung die im if festgelegt ist nicht wahr ist.
+
+let age = 14;
+
+if (age >= 18) {
+  console.log(" du darfst rein");
+} else if (age <= 17 && age >= 14) {
+  console.log("Netter Versuch !!!");
+} else {
+  console.log("du kommst hier nicht rein");
+}
+
+//* Login
+
+let username = "Lisa";
+let password = "Supercode";
+function checkUserData() {
+  const loginUserName = document.querySelector("#username").value;
+  const loginUserPw = document.querySelector("#password").value;
+
+  const outputLogin = document.querySelector("#login-feedback");
+  if (username === loginUserName && password === loginUserPw) {
+    outputLogin.innerHTML = "Login erfolgreich";
+  } else {
+    outputLogin.innerHTML = "Du bist zu blöd um deine Daten einzugeben";
+  }
 }
