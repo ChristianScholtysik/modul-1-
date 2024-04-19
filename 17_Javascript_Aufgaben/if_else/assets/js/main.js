3d7a9bb4a621e9eb7fa91bab6bdcf2a0dd93fb9d
Christian Scholtysik <christianscholtysik@gmail.com>
2024-04-19 16:16:06 +0200

    done

--- 17_Javascript_Aufgaben/if_else/assets/js/main.js
+++ 17_Javascript_Aufgaben/if_else/assets/js/main.js
@@ -42,3 +42,40 @@ function shisha() {
 }
 
 //Conditionals-Level-2_2
+
+function checkAirQuality() {
+  const range = Number(document.querySelector("#air").value);
+  const reihe1 = document.querySelector("#row1");
+  const reihe2 = document.querySelector("#row2");
+  const hintergrund = document.querySelector("body");
+
+  if (range <= 50) {
+    reihe1.innerHTML = `Level of health concern:  Good`;
+    reihe2.innerHTML = `effect: Little or no risk`;
+    hintergrund.style.backgroundColor = "lightgreen";
+  } else if (range >= 50 && range <= 100) {
+    reihe1.innerHTML = `Level of health concern:  Moderate Level of health`;
+    reihe2.innerHTML = `effect: Acceptable quality`;
+    hintergrund.style.backgroundColor = "yellow";
+  } else {
+    reihe1.innerHTML = `Level of health concern:  Unhealthy for sensitive groups`;
+    reihe2.innerHTML = `effect: Generable publics not likely affected`;
+    hintergrund.style.backgroundColor = "red";
+  }
+}
+
+//Conditionals-Level-2_3
+
+function twentySeven(zahl1) {
+  let differenz = 27 - zahl1;
+
+  if (zahl1 > 27) {
+    differenz = zahl1 - 27;
+    const diff2 = differenz * 2;
+    console.log(diff2);
+  } else {
+    console.log(differenz);
+  }
+}
+
+twentySeven(26);
